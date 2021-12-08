58eaffe7df51b1ec442b12729ebb223b60c6e187
Trevor Chaney <trevorchaney@gmail.com>
2021-12-07 19:45:35 -0600

    Testing on WSL

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/bash
 
-set -xe
+# set -xe
 
 # Install nix
 curl -L https://nixos.org/nix/install | sh
 
 # Source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 
 # Install packages
 nix-env -iA \
     nixpkgs.antibody \
     nixpkgs.bat \
-    nixpkgs.clang \
+    nixpkgs.clang_13 \
     nixpkgs.direnv \
     nixpkgs.doas \
     nixpkgs.fzf \
-    nixpkgs.gcc \
     nixpkgs.git \
     nixpkgs.gnumake \
     nixpkgs.jump \
@@ -30,18 +29,21 @@ nix-env -iA \
 
 # Install dotfiles with gnu stow
 stow git
-stow nvim
-stow tmux
+#stow nvim
+#stow tmux
 stow vim
 stow shells
 
 # Add nix.zsh shell to login shells
-if [ ! -z $(grep "zsh" "/etc/shells") ]; then
+if grep -q "zsh" "/etc/shells"; then
     echo "zsh was found in /etc/shells";
 else
     echo "Adding zsh to /etc/shells"
     command -v zsh | sudo tee -a /etc/shells;
 fi
+
+## Install Oh-My-Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Run the following to make zsh the user's login shell
 #$ sudo chsh -s `which zsh` $USER
@@ -50,7 +52,7 @@ fi
 # antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
 # Make a symbolic link to vim config for neovim config
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+ln -s "$HOME/.vimrc" "$HOME/.config/nvim/init.vim"
 
 # Install neovim plugins
 nvim --headless +PlugInstall +qall
