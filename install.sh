5a05e2206b18a4bcf789ed0e27496751613b3855
Trevor Chaney <trevorchaney@gmail.com>
2022-01-05 17:22:47 -0600

    Update from RHEL

--- install.sh
+++ install.sh
@@ -5,50 +5,59 @@
 # Install packages if the development environment is not manjaro/arch
 # looking at you RHEL.
 if ! grep -qE "manjaro|arch" "/etc/os-release"; then
-  echo "I guess we are doing this the hard way"
-  echo "Installing nix package manager and packages"
-  # Install nix
-  curl -L https://nixos.org/nix/install | sh
+    echo "I guess we're doing this the hard way..."
+    echo "Installing nix package manager and packages"
+    # Install nix
+    curl -L https://nixos.org/nix/install | sh
 
   # Source nix
   . ~/.nix-profile/etc/profile.d/nix.sh
 
   nix-env -iA \
-    nixpkgs.antibody \
-    nixpkgs.bat \
-    nixpkgs.bpytop \
-    nixpkgs.clang_13 \
-    nixpkgs.direnv \
-    nixpkgs.fzf \
-    nixpkgs.gdb \
-    nixpkgs.git \
-    nixpkgs.global \
-    nixpkgs.gnumake \
-    nixpkgs.htop \
-    nixpkgs.jump \
-    nixpkgs.neovim \
-    nixpkgs.nodejs \
-    nixpkgs.ranger \
-    nixpkgs.ripgrep \
-    nixpkgs.silver-searcher \
-    nixpkgs.stow \
-    nixpkgs.tmux \
-    nixpkgs.toilet \
-    nixpkgs.valgrind \
-    nixpkgs.vim \
-    nixpkgs.yarn \
-    nixpkgs.zsh
+      nixpkgs.antibody \
+      nixpkgs.bat \
+      nixpkgs.bpytop \
+      nixpkgs.clang_13 \
+      nixpkgs.direnv \
+      nixpkgs.fzf \
+      nixpkgs.gdb \
+      nixpkgs.git \
+      nixpkgs.global \
+      nixpkgs.gnumake \
+      nixpkgs.htop \
+      nixpkgs.jump \
+      nixpkgs.neovim \
+      nixpkgs.nodejs \
+      nixpkgs.ranger \
+      nixpkgs.ripgrep \
+      nixpkgs.silver-searcher \
+      nixpkgs.stow \
+      nixpkgs.tmux \
+      nixpkgs.toilet \
+      nixpkgs.valgrind \
+      nixpkgs.vim \
+      nixpkgs.yarn \
+      nixpkgs.zsh
+
+    # Add nix.zsh shell to login shells
+    if grep -q "zsh" "/etc/shells"; then
+        echo "zsh was found in /etc/shells";
+    else
+        echo "Adding zsh to /etc/shells"
+        command -v zsh | sudo tee -a /etc/shells;
+    fi
+
 else
-  echo "Thank goodness, I thought you were a troglodyte"
+    echo "Thank goodness, I thought you were a troglodyte"
 fi
 
 # Install vim-plug if it isn't already installed
 if [ -f ~/.vim/autoload/plug.vim ]; then
-  echo "Installing vim-plug"
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "Installing vim-plug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
-  echo "Found vim-plug, already installed"
+    echo "Found vim-plug, already installed"
 fi
 
 
@@ -67,14 +76,6 @@ stow vim
 [ -d ~/.config/nvim/tmp ] && mkdir -p ~/.config/nvim/tmp
 [ -d ~/.cache/vim/ctags ] && mkdir -p ~/.cache/vim/ctags
 
-# Add nix.zsh shell to login shells
-if grep -q "zsh" "/etc/shells"; then
-    echo "zsh was found in /etc/shells";
-else
-    echo "Adding zsh to /etc/shells"
-    command -v zsh | sudo tee -a /etc/shells;
-fi
-
 # Install zsh plugins
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
@@ -92,7 +93,8 @@ mkdir -p "$HOME/.config/nvim"
 
 # Make a symbolic link to vim-plug install in .vim directory
 mkdir -p "$HOME/.local/share/nvim/site/autoload"
-[ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ] && ln -s "$HOME/.vim/autoload/plug.vim" "$HOME/.local/share/nvim/site/autoload/plug.vim"
+[ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ] \
+    && ln -s "$HOME/.vim/autoload/plug.vim" "$HOME/.local/share/nvim/site/autoload/plug.vim"
 
 # Install neovim plugins
 nvim --headless +PlugInstall +qall
